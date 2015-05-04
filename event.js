df1dfcfba03f92534cc439a6b05d7a28e72c2ef6
Gilberto Leon <ripflame@gmail.com>
2015-05-04 12:39:38 -0500

    Adding token handling to event.js

--- event.js
+++ event.js
@@ -6,59 +6,59 @@ module.exports = function(RED) {
 
     self.server = RED.nodes.getNode(config.server);
 
-    self.host        = self.server.host;
-    self.secure      = self.server.secure;
-    self.port        = self.server.port;
     self.device      = config.device;
     self.altairEvent = config.altairEvent;
 
     if (self.server) {
-      self.token = self.server.credentials.token;
+
+      self.server.on('tokenReady', function(token) {
+        self.token = token;
+
+        var url;
+        if (self.server.secure) {
+          url = 'https://' + self.server.host + ':' + self.server.port;
+        } else {
+          url = 'http://' + self.server.host + ':' + self.server.port;
+        }
+
+        var io = require('socket.io-client');
+        self.socket = io(url, {
+              query       : "token=" + self.token,
+              autoconnect : true
+            });
+
+        self.socket.on("connect", function() {
+          console.log("Socket connected");
+        });
+
+        self.socket.on("error", function(err) {
+          console.log("Socket connection error: ", err);
+        });
+
+        self.socket.on('event', function(device, eventN, param) {
+          for ( var aEvent in device.events ) {
+            if (device.events[aEvent].name === self.altairEvent && eventN === device.events[aEvent].n) {
+              var msg = {
+                payload: {
+                  "device" : device,
+                  "eventN" : eventN,
+                  "param"  : param
+                }
+              };
+              self.send(msg);
+            }
+          }
+        });
+      });
+
     } else {
       console.log('Server undefined');
     }
 
-    var io = require('socket.io-client');
-
-    var url;
-    if (self.secure) {
-      url = 'https://' + self.host + ':' + self.port;
-    } else {
-      url = 'http://' + self.host + ':' + self.port;
-    }
-
-    var socket = io(url, {
-          query       : "token=" + self.token,
-          autoconnect : true
-        });
-
-    socket.on("connect", function() {
-      console.log("Socket connected");
-    });
-
-    socket.on("error", function(err) {
-      console.log("Socket connection error: ", err);
-    });
-
-    socket.on('event', function(device, eventN, param) {
-      for ( var aEvent in device.events ) {
-        if (device.events[aEvent].name === self.altairEvent && eventN === device.events[aEvent].n) {
-          var msg = {
-            payload: {
-              "device" : device,
-              "eventN" : eventN,
-              "param"  : param
-            }
-          };
-          self.send(msg);
-        }
-      }
-    });
-
     self.on('close', function() {
-      socket.removeAllListeners('event');
-      socket.removeAllListeners('error');
-      socket.removeAllListeners('connect');
+      self.socket.removeAllListeners('event');
+      self.socket.removeAllListeners('error');
+      self.socket.removeAllListeners('connect');
     });
   }
 
